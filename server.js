4dca40423bcc0753140eb7d70a89d6f08523ba39
Vancios86 <albuion86@gmail.com>
2020-11-20 14:35:45 +0000

    adding the server project

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signIn = require('./controllers/signIn');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '123',
+    database : 'brain-db'
+  }
+});
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+})
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => { res.send(database.users) } )
+
+app.post('/signIn', (req, res) => { signIn.handleSignIn(req, res, db, bcrypt) } )
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) } )
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) } )
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) } )
+
+app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) } )
+
+
+app.listen(3001, ()=>{
+	console.log('app is running on 3001');
+})
+
+
