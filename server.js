8ab198dc9d27765735c76f6565aee52948bebcf3
Vancios86 <albuion86@gmail.com>
2020-11-24 17:05:56 +0000

    fixing the database host

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-infinite-80550',
-    user : 'postgres',
-    password : '',
-    database : 'brain-db'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
